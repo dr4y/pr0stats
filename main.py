4136385e0c11a78ebd1e544d47304a5e1d032058
Christian Dresen <c.dresen@fh-muenster.de>
2017-09-12 01:25:12 +0200

    Added smart config file reader. Config can now be at /etc/pr0stats.ini, /etc/pr0stats/config.ini or at the script path

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
-import json
+import json, os, sys
 import requests
 import configparser
+from pathlib import Path
 from extensions.chartjs.ChartJsGenerator import ChartJsGenerator
 
-
+script_path = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
-config.read('config.ini')
+
+if Path("/etc/pr0stats.ini").is_file():
+    config.read('/etc/pr0stats.ini')
+elif Path("/etc/pr0stats/config.ini").is_file():
+    config.read("/etc/pr0stats/config.ini")
+elif Path(script_path+"/config.ini").is_file():
+    config.read(script_path+'/config.ini')
+else:
+    sys.stderr.write("Config-File not found.")
+    sys.exit(1)
 
 api = "https://pr0gramm.com/api/profile/info?name={}&flags=4".format(config['general']['username'])
 resp = json.loads(requests.get(api).text)
